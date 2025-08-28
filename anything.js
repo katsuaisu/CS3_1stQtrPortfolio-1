78bbb77901c0ecabf127680aac605d0b33aaac71
b2029xecyu@pshs.edu.ph <b2029xecyu@pshs.edu.ph>
2025-08-28 16:04:41 +0800

    crippled 27

--- anything.js
+++ anything.js
@@ -1,2 +1,37 @@
 console.group('Hi STRONTIUM!');
 
+//gma - 7
+//abs - cbn - 2
+//tv5 - 5
+//ibc - 13
+//ptv - 4
+
+rl = require('readline') //
+rl1 = rl.createInterface(
+    {
+        input: process.stdin,  
+    }
+)
+rli.on('line', give_station)
+
+function give_station(num)
+{
+    switch (Number(num))
+    {
+        case 7:
+            console.log('gma - 7')
+            break
+        case 2:
+            console.log('abs - cbn - 2')
+            break
+        case 5:
+            console.log('tv5 - 5')
+            break
+        case 13:
+            console.log('ibc - 13')
+            break
+        case 4:
+            console.log('ptv - 4')
+            break
+    }
+}
